fd702471087aa98eff9c5aaf73790623b0460a87
Carlos-Hung-Feng <carloshung1998@gmail.com>
2024-02-12 19:24:01 -0400

    feat: Add comment to functions

--- index.js
+++ index.js
@@ -1,27 +1,35 @@
+// Establecer la conexión con el servidor de Socket.IO
 const socket = io();
 
-var userName;
+var userName; // Variable para almacenar el nombre de usuario
 
+// Función para encontrar un jugador y comenzar el juego
 const findPlayer = () => {
-    userName = document.getElementById("name").value;
+    userName = document.getElementById("name").value; // Obtener el nombre de usuario del input
 
-    document.getElementById("user").innerText = userName;
+    document.getElementById("user").innerText = userName; // Mostrar el nombre de usuario en la interfaz
 
+    // Verificar si se proporcionó un nombre de usuario válido
     if(userName === null || userName === ''){
         alert("Se necesita un nombre para iniciar la partida.");
     }
     else{
+        // Emitir un evento 'find' con el nombre de usuario al servidor
         socket.emit('find', {name:userName});
 
+        // Mostrar el contenido de carga en la interfaz
         showContentById('loading')
 
+        // Deshabilitar el botón de búsqueda para evitar múltiples solicitudes
         document.getElementById('find').disabled=true;
     }
 }
 
+// Manejar el evento 'find' del servidor
 socket.on('find', (e) => {
     let playersArray = e.players
     
+    // Encontrar la partida en la que está involucrado el usuario actual
     const foundObj = playersArray.find(obj => obj.p1.name === `${userName}` || obj.p2.name === `${userName}`)
     
     if(foundObj == null){
@@ -30,6 +38,7 @@ socket.on('find', (e) => {
 
     console.log(playersArray);
 
+    // Mostrar los elementos relacionados con el juego en la interfaz
     showContentById('userCont');
     showContentById('oppUserCont');
     showContentById('valueCont');
@@ -37,24 +46,28 @@ socket.on('find', (e) => {
     showContentById('container');
     showContentById('turn');
 
+    // Ocultar el contenido de carga y los elementos relacionados con la entrada de nombre
     hideContentById('loading');
     hideContentById('name');
     hideContentById('find');
     hideContentById('nameTextHint');
 
+    // Establecer el mensaje de turno para el jugador 'X'
     document.getElementById('turn').innerText = 'Turno de X';
 
     let oppName
     let value
 
-
+    // Determinar el oponente y el valor del jugador actual
     foundObj.p1.name === userName ? oppName = foundObj.p2.name : oppName = foundObj.p1.name;
     foundObj.p2.name === userName ? value = foundObj.p2.value : value = foundObj.p1.value;
 
+    // Mostrar el nombre del oponente y el valor del jugador actual en la interfaz
     document.getElementById('oppUser').innerText = oppName;
     document.getElementById('value').innerText = value;
     document.getElementById('oppnValue').innerText = value == 'O' ? 'X' : 'O';
     
+    // Habilitar o deshabilitar los botones según el turno del jugador actual
     if (value != document.getElementById('turn').innerText.split(' ').slice(-1)){
         changeButtonState(true);
     }
@@ -63,15 +76,18 @@ socket.on('find', (e) => {
     }
 })
 
+// Agregar un event listener a todos los botones del juego
 document.querySelectorAll('.btn').forEach(e => {
     e.addEventListener('click', function() {
         let value = document.getElementById('value').innerText;
         e.innerText = value;
 
+        // Emitir un evento 'playing' con la jugada actual al servidor
         socket.emit('playing', {value: value, id: e.id, name: userName});
     })
 })
 
+// Manejar el evento 'playing' del servidor
 socket.on('playing', (e) => {
 
     const foundObj = (e.players).find(obj => obj.p1.name === `${userName}` || obj.p2.name === `${userName}`)
@@ -83,6 +99,7 @@ socket.on('playing', (e) => {
     p1MarkedPosition = foundObj.p1.markedPosition;
     p2MarkedPosition = foundObj.p2.markedPosition;
 
+    // Establecer el mensaje de turno según el estado del juego
     if ((foundObj.sum) % 2 === 0){
         document.getElementById('turn').innerText = 'Turno de O';
     }
@@ -90,6 +107,7 @@ socket.on('playing', (e) => {
         document.getElementById('turn').innerText = 'Turno de X';
     }
 
+    // Mostrar las jugadas realizadas por los jugadores en la interfaz
     if (p1MarkedPosition != ''){
         document.getElementById(`${p1MarkedPosition}`).innerText='X';
         document.getElementById(`${p1MarkedPosition}`).disabled = true;
@@ -99,7 +117,7 @@ socket.on('playing', (e) => {
         document.getElementById(`${p2MarkedPosition}`).disabled = true;
     }
 
-    
+    // Habilitar o deshabilitar los botones según el turno del jugador actual
     if (document.getElementById('value').innerText != document.getElementById('turn').innerText.split(' ').slice(-1)){
         changeButtonState(true);
     }
@@ -107,9 +125,11 @@ socket.on('playing', (e) => {
         changeButtonState(false);
     }
 
+    // Verificar si hay un ganador o empate
     check(userName, foundObj.sum);
 })
 
+// Función para cambiar el estado de los botones
 const changeButtonState = (_disabled) => {
     document.getElementById('btn1').innerText == '' ? document.getElementById('btn1').disabled = _disabled : document.getElementById('btn1').disabled = true;
     document.getElementById('btn2').innerText == '' ? document.getElementById('btn2').disabled = _disabled : document.getElementById('btn2').disabled = true;
@@ -122,6 +142,7 @@ const changeButtonState = (_disabled) => {
     document.getElementById('btn9').innerText == '' ? document.getElementById('btn9').disabled = _disabled : document.getElementById('btn9').disabled = true;
 }
 
+// Función para verificar si hay un ganador o empate
 const check = (name, sum) => {
     let btn1 = document.getElementById('btn1').innerText;
     let btn2 = document.getElementById('btn2').innerText;
@@ -133,6 +154,7 @@ const check = (name, sum) => {
     let btn8 = document.getElementById('btn8').innerText;
     let btn9 = document.getElementById('btn9').innerText;
 
+    // Verificar si hay un ganador en cualquier fila, columna o diagonal
     if ((btn1 != '' && btn1 == btn2 && btn2 == btn3) || // row 1
         (btn4 != '' && btn4 == btn5 && btn5 == btn6) || // row 2
         (btn7 != '' && btn7 == btn8 && btn8 == btn9) || // row 3
@@ -142,9 +164,11 @@ const check = (name, sum) => {
         (btn1 != '' && btn1 == btn5 && btn5 == btn9) || // diagonal \
         (btn3 != '' && btn3 == btn5 && btn5 == btn7)){  // diagonal /
         
+        // Emitir un evento 'gameOver' con el nombre del jugador al servidor
         socket.emit('gameOver', {name: name});
         
         let winner;
+        // Determinar el ganador y mostrarlo en la interfaz
         if (sum % 2 == 0) {
             let userValue = document.getElementById('value').innerText;
             if (userValue == 'X'){
@@ -173,28 +197,35 @@ const check = (name, sum) => {
         changeButtonState(true);
     }
     else if (sum === 10){
+        // Emitir un evento 'gameOver' con el nombre del jugador al servidor
         socket.emit('gameOver', {name: name});
         
+        // Mostrar el mensaje de empate en la interfaz
         document.getElementById('turn').innerText = 'Empate 🤷‍♂️';
         showContentById('refresh');
         changeButtonState(true);
     }
 }
 
+// Manejar el evento 'nextPair' del servidor
 socket.on('nextPair', (e) => {
 
+    // Emitir un evento 'nextPair' al servidor
     socket.emit('nextPair', e);
 })
 
+// Función para refrescar la página
 const refreshPage = () => {
     location.reload();
 }
 
+// Función para mostrar un elemento por su ID
 const showContentById = (tagId) => {
     document.getElementById(`${tagId}`).classList.remove("hide");
     document.getElementById(`${tagId}`).classList.add("show");
 }
 
+// Función para ocultar un elemento por su ID
 const hideContentById = (tagId) => {
     document.getElementById(`${tagId}`).classList.remove("show");
     document.getElementById(`${tagId}`).classList.add("hide");
--- server.js
+++ server.js
@@ -1,32 +1,41 @@
-const express = require('express');
+// Importar las dependencias necesarias
+const express = require('express'); 
 const app = express();
 
-const path = require('path');
-const http = require('http');
-const {Server} = require('socket.io');
+const path = require('path'); // Importar el módulo 'path' para manejar rutas de archivos
+const http = require('http'); // Importar el módulo 'http' de Node.js para crear el servidor HTTP
+const {Server} = require('socket.io'); // Importar la clase 'Server' de Socket.IO para la comunicación en tiempo real
 
+// Crear un servidor HTTP utilizando la aplicación Express
 const server = http.createServer(app);
 
+// Crear una instancia de Socket.IO que escuche en el servidor HTTP
 const io = new Server(server);
+
+// Configurar Express para servir archivos estáticos desde la carpeta raíz del proyecto
 app.use(express.static(path.resolve('')));
 
+// Definir una ruta para la página de inicio
 app.get('/', (req, res) => {
-    return res.sendFile('index.html');
+    return res.sendFile('index.html'); // Enviar el archivo 'index.html' al cliente
 });
 
-const playingLimit = 3; // aqui controla la cantidad de partidas que se puede jugar
+const playingLimit = 3; // Controlar la cantidad máxima de partidas que se pueden jugar simultáneamente.
 
-let userNameArray = [];
-let playingArray = [];
-let waittingArray = [];
-let removedFromWaitting = false;
+let userNameArray = []; // Array para almacenar los nombres de usuario
+let playingArray = []; // Array para almacenar las partidas en curso
+let waitingArray = []; // Array para almacenar las partidas en espera
+let removedFromWaiting = false; // Bandera para controlar si se ha eliminado un usuario de las partidas en espera
 
+// Manejar la conexión de un cliente a través de Socket.IO
 io.on('connection', (socket) => {
+    // Manejar el evento 'find' para buscar una partida disponible
     socket.on('find', (e) => {
         if(e.name !== null){
             userNameArray.push(e.name);
 
             if(userNameArray.length >= 2){
+                // Crear objetos para los dos jugadores de la partida
                 let p1obj = {
                     name: userNameArray[0],
                     value: 'X',
@@ -44,28 +53,31 @@ io.on('connection', (socket) => {
                     sum: 1
                 }
 
+                // Verificar si hay espacio disponible para una nueva partida
                 if(playingArray.length < playingLimit){ 
                     playingArray.push(obj);
                     console.log(`Matched: ${obj.p1.name} vs ${obj.p2.name}`);
-                    io.emit('find', {players: playingArray});
+                    io.emit('find', {players: playingArray}); // Emitir un evento con la información de las partidas en curso
                 }
                 else{
-                    waittingArray.push(obj);
+                    waitingArray.push(obj); // Agregar la partida al array de partidas en espera
                 }
 
-                userNameArray.splice(0,2)
+                userNameArray.splice(0,2) // Eliminar los nombres de usuario utilizados de la lista
             }
         }
     })
 
+    // Manejar el evento 'nextPair' para emparejar a los jugadores en espera con nuevas partidas disponibles
     socket.on('nextPair', (e) => {
         if(e !== undefined && playingArray.length < playingLimit){
-            playingArray.push(e.nextPair);
+            playingArray.push(e.nextPair); // Agregar la nueva partida al array de partidas en curso
             console.log(`Matched: ${e.nextPair.p1.name} vs ${e.nextPair.p2.name}`);
-            io.emit('find', {players: playingArray});
+            io.emit('find', {players: playingArray}); // Emitir un evento con la información de las partidas en curso
         }
     })
 
+    // Manejar el evento 'playing' para actualizar el estado de la partida en curso
     socket.on('playing', (e) => {
         if(e.value === 'X'){
             let objToChange = playingArray.find(obj => obj.p1.name === e.name);
@@ -79,20 +91,21 @@ io.on('connection', (socket) => {
             objToChange.sum++;
         }
 
-        io.emit('playing', {players: playingArray});
+        io.emit('playing', {players: playingArray}); // Emitir un evento con la información actualizada de las partidas en curso
     })
 
+    // Manejar el evento 'gameOver' para finalizar una partida
     socket.on('gameOver', (e) => {
-        playingArray = playingArray.filter(obj => obj.p1.name != e.name);
+        playingArray = playingArray.filter(obj => obj.p1.name != e.name); // Eliminar la partida finalizada del array de partidas en curso
 
-        if (waittingArray.length > 0){
-            if (removedFromWaitting === false){
-                let nextPair = waittingArray.at(0);
-                waittingArray = waittingArray.filter(obj => obj.p1.name != nextPair.p1.name);
-                io.emit('nextPair', {nextPair: nextPair});
+        if (waitingArray.length > 0){
+            if (removedFromWaiting === false){
+                let nextPair = waitingArray[0]; // Obtener la próxima partida en espera
+                waitingArray = waitingArray.filter(obj => obj.p1.name != nextPair.p1.name); // Eliminar la partida emparejada del array de partidas en espera
+                io.emit('nextPair', {nextPair: nextPair}); // Emitir un evento para emparejar a los jugadores en espera con la nueva partida disponible
             }
             
-            removedFromWaitting = !removedFromWaitting;
+            removedFromWaiting = !removedFromWaiting; // Cambiar el estado de la bandera
         }
     })
 
@@ -102,6 +115,7 @@ io.on('connection', (socket) => {
     });
 });
   
+// Escuchar en el puerto 3000
 server.listen(3000, () => {
     console.log('server running at http://localhost:3000');
 });
